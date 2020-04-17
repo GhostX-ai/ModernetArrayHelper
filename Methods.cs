4085d0cea535019580fc006eeb252e35e7804194
GhostX <egamovkhurshed29@gmail.com>
2020-04-17 19:18:05 +0500

    Push has been done

--- Methods.cs
+++ Methods.cs
@@ -12,4 +12,14 @@ class Methods<M>
         ar = nar;
         return end;
     }
+    public void Push(ref M[] ar, M ne)
+    {
+        M[] nar = new M[ar.Length + 1];
+        for (int x = 0; x < ar.Length; x++)
+        {
+            nar[x] = ar[x];
+        }
+        nar[nar.Length - 1] = ne;
+        ar = nar;
+    }
 }
